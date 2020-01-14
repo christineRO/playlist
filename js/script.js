e7f9aad5c67aa864517e93612a4746207471a421
christineRO <christinerole@icloud.com>
2020-01-14 02:20:42 +0000

    do css:(

--- js/script.js
+++ js/script.js
@@ -6,44 +6,52 @@ let lengths = ['516'];
 let images = ['https://i.ytimg.com/vi/sdDiHZiBtpE/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCVXJk4hO5ELcVBecX4221-bCHWWw'];
 let links = ['https://www.youtube.com/watch?v=sdDiHZiBtpE'];
 
+update();
+
+$("#addSongInput").click(function(){
+    let userSong = $("#userSongInput").val();
+    songs.push(userSong);
+
+    let userArtist = $("#userArtistInput").val();
+    artists.push(userArtist);
+
+    let userLength = $("#userLengthInput").val();
+    lengths.push(userLength);
+
+    let userImage = $("#userImageInput").val();
+    images.push(userImage);
+
+    let userLink = $("#userLinkInput").val();
+    links.push(userLink);
+
+    update();
+});
+
+function update(){
+$(".songDisplay").empty();
+$(".artistDisplay").empty();
+$(".lengthDisplay").empty();
+$(".imageDisplay").empty();
+$(".linkDisplay").empty();
+
 songs.forEach(function(song){
-    $(".playlistDisplay").append(`<li>${song}</li>`);
+    $(".songDisplay").append(`<li>${song}</li>`);
 });
 
 artists.forEach(function(artist){
-    $(".playlistDisplay").append(`<li>${artist}</li>`);
+    $(".artistDisplay").append(`<li>${artist}</li>`);
 });
 
 lengths.forEach(function(length){
-    $(".playlistDisplay").append(`<li>${length}</li>`);
+    $(".lengthDisplay").append(`<li>${length}</li>`);
 });
 
 images.forEach(function(image){
-    $(".playlistDisplay").append(`<li><img src = "${image}"></li>`);
+    $(".imageDisplay").append(`<li><img src = "${image}"></li>`);
 });
 
 links.forEach(function(link){
-    $(".playlistDisplay").append(`<li><a href = "${link}" target="_blank">Link</a></li>`);
+    $(".linkDisplay").append(`<li><a href = "${link}" target="_blank">Link</a></li>`);
 });
-
-$("#addSong").click(function(){
-    let userSong = $("#userSong").val();
-    songs.push(userSongs);
-
-    let userArtist = $("#userArtist").val();
-    artists.push(userArtist);
-
-    let userLength = $("#userLength").val();
-    lengths.push(userLength);
-
-    $("#userImage").val();
-    $("#userLink").val();
-});
-
-    
-    
-    
-    images.push(userImage);
-    links.push(userLink);
-
+};
     console.log(songs);
