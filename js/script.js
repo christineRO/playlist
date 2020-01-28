dd0ddbeb3e8c5be9a580177e281f13d74f3769f8
christineRO <christinerole@icloud.com>
2020-01-28 02:26:57 +0000

    Cool; Cleanup

--- js/script.js
+++ js/script.js
@@ -1,41 +1,72 @@
+// ignore the notes lol
 
-
-let songs = ['Rivers in the Desert'];
+/* let songs = ['Rivers in the Desert'];
 let artists = ['Lyn Okamoto'];
 let lengths = ['516'];
 let images = ['https://i.ytimg.com/vi/sdDiHZiBtpE/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCVXJk4hO5ELcVBecX4221-bCHWWw'];
 let links = ['https://www.youtube.com/watch?v=sdDiHZiBtpE'];
+*/
 
-update();
+let songInfo = {
+    names: "Rivers in the Desertpp",
+    artists: "Lyn Okamoto",
+    lengths: "516",
+    images: "https://i.ytimg.com/vi/sdDiHZiBtpE/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCVXJk4hO5ELcVBecX4221-bCHWWw",
+    links: "https://www.youtube.com/watch?v=sdDiHZiBtpE"
+};
+
+let songInfoTwo = {
+    names: "Rivers in the Desert",
+    artists: "Lyn Okamoto",
+    lengths: "516",
+    images: "https://i.ytimg.com/vi/sdDiHZiBtpE/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCVXJk4hO5ELcVBecX4221-bCHWWw",
+    links: "https://www.youtube.com/watch?v=sdDiHZiBtpE"
+};
+
+let songList = [songInfo, songInfoTwo];
+
+console.log("WOOOOO")
 
 $("#addSongInput").click(function(){
     let userSong = $("#userSongInput").val();
-    songs.push(userSong);
+    songInfo.push(songInfo.names);
 
     let userArtist = $("#userArtistInput").val();
-    artists.push(userArtist);
+    songInfo.push(songInfo.artists);
 
     let userLength = $("#userLengthInput").val();
-    lengths.push(userLength);
+    songInfo.push(songInfo.lengths);
 
     let userImage = $("#userImageInput").val();
-    images.push(userImage);
+    songInfo.push(songInfo.images);
 
     let userLink = $("#userLinkInput").val();
-    links.push(userLink);
+    songInfo.push(songInfo.links);
 
     update();
 });
 
 function update(){
-$(".songDisplay").empty();
-$(".artistDisplay").empty();
-$(".lengthDisplay").empty();
-$(".imageDisplay").empty();
-$(".linkDisplay").empty();
+    $(".songDisplay").empty();
+    $(".artistDisplay").empty();
+    $(".lengthDisplay").empty();
+    $(".imageDisplay").empty();
+    $(".linkDisplay").empty();
 
-songs.forEach(function(song){
-    $(".songDisplay").append(`<li>${song}</li>`);
+    console.log(songList);
+
+    songList.forEach(function(item){
+        $(".songDisplay").append(`<li>${item["names"]}</li>`);
+        $(".artistDisplay").append(`<li>${item["artists"]}</li>`);
+        $(".lengthDisplay").append(`<li>${item["lengths"]}</li>`);
+        $(".imageDisplay").append(`<li><img src = "${item["images"]}></li>`);
+        $(".linkDisplay").append(`<li><a href = "${item["links"]}" target="_blank">Link</a></li>`);
+        console.log("Hello");
+    });
+}
+update();
+/* songInfo.forEach(function(song){
+    $(".songDisplay").append(`<li>${name}</li>`);
 });
 
 artists.forEach(function(artist){
@@ -54,4 +85,4 @@ links.forEach(function(link){
     $(".linkDisplay").append(`<li><a href = "${link}" target="_blank">Link</a></li>`);
 });
 };
-    console.log(songs);
+*/
